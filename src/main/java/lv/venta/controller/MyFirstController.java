844aee89a9be96f7e8be72ce1bb07b40076b42db
KarinaSkirmante <karina.krinkele@gmail.com>
2025-02-15 09:49:23 +0200

    Initial: New project for seminar 1 with simple controller

---
+++ src/main/java/lv/venta/controller/MyFirstController.java
@@ -0,0 +1,27 @@
+package lv.venta.controller;
+
+import java.util.Random;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyFirstController {
+	
+	@GetMapping("/simple") //localhost:8080/simple
+	public String getControllerSimple()
+	{
+		System.out.println("Ir izsaukusies simple funkcija");
+		return "simple-page";//parādīs  simple-page.html
+	}
+	@GetMapping("/getdata") //localhost:8080/getdata
+	public String getControllerGetData(Model model) {
+		System.out.println("Ir izsaukusies get data funkcija");
+		Random rand = new Random();
+		String data = "Karina: " + rand.nextInt(0, 100); 
+		model.addAttribute("package", data);
+		return "data-page";//parādīs data-page.html
+	}
+
+}
