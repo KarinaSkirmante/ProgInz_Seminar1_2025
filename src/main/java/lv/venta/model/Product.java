2b7ec114e80548017b759a605af004a02b8de999
KarinaSkirmante <karina.krinkele@gmail.com>
2025-02-15 11:33:26 +0200

    Add: added lombok and complete product class

--- src/main/java/lv/venta/model/Product.java
+++ src/main/java/lv/venta/model/Product.java
@@ -1,11 +1,39 @@
 package lv.venta.model;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class Product {
+	@Setter(value = AccessLevel.NONE)
 	private int id;
+	
 	private String title;
 	private String description;
 	private float price;
 	private int quantiy;
 	
-
+	@Setter(value = AccessLevel.NONE)
+	@Getter(value = AccessLevel.NONE)
+	private static int counter = 0;
+	
+	public void setId() {
+		id = counter;
+		counter++;
+	}
+	
+	public Product(String inputTitle, String inputDescription, float inputPrice, int inputQuantity)
+	{
+		setId();
+		setTitle(inputTitle);
+		setDescription(inputDescription);
+		setPrice(inputPrice);
+		setQuantiy(inputQuantity);
+	}
 }
